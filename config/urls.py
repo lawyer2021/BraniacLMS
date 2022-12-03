a133576abdf658af9800f4c23dc7faec23bf7cb4
lawyer2021 <gconsul@mail.com>
2022-12-03 23:55:06 +0300

    Django Template System (base templates, other templates, menu-templates, output cycle of news, paginator for news page, templates filter for type mailto)

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls')),
+    # path('', include('mainapp.urls')),
+    path("", RedirectView.as_view(url="mainapp/")),
+    path('mainapp/', include('mainapp.urls', namespace='mainapp')),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from datetime import datetime
 
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
@@ -18,3 +19,17 @@ class LoginView(TemplateView):
 
 class NewsView(TemplateView):
     template_name = 'mainapp/news.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news_title'] = "Новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context['range'] = range(5)
+        context['datetime_obj'] = datetime.now()
+        return context
+
+class NewsViewPaginator(NewsView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context['page_num'] = page
+        return context
